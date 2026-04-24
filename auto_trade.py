fbe3d0bc36f6d472e34c1275c393a6658add3361
Johannes Haq <139051782+Dhiyaahaq33@users.noreply.github.com>
2026-04-24 10:52:10 +0700

    Change default port from 5000 to 8000

--- auto_trade.py
+++ auto_trade.py
@@ -192,7 +192,7 @@ def get_intelligence():
 
 if __name__ == "__main__":
     fetch_all_markets()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     threading.Thread(target=whale_and_anomaly_detector, daemon=True).start()
     threading.Thread(target=lambda: bot.infinity_polling(), daemon=True).start()
     app.run(host='0.0.0.0', port=port)
